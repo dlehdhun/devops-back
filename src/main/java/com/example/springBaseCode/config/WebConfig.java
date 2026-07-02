60b43b4cd2713278e85091545d9f9264fc399be0
LDH <leedoh03@naver.com>
2026-07-02 16:42:06 +0900

    cors설정

--- src/main/java/com/example/springBaseCode/config/WebConfig.java
+++ src/main/java/com/example/springBaseCode/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:3000",   // 로컬 개발 (npm start)
                         "http://localhost",        // Docker 배포 (Nginx 80)
                         "http://localhost:80",
-                        "http://15.164.163.6"   // ← EC2 IP 추가
+                        "http://13.203.134.29"   // ← EC2 IP 추가
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
